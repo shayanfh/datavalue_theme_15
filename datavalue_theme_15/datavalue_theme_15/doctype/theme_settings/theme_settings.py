82ca7b9e5a7b4191748f07e98a729d5cc57ec5dc
shayanfh <shayan.famous79@gmail.com>
2026-04-29 13:55:14 +0330

    add get_theme_settings

--- datavalue_theme_15/datavalue_theme_15/doctype/theme_settings/theme_settings.py
+++ datavalue_theme_15/datavalue_theme_15/doctype/theme_settings/theme_settings.py
@@ -6,3 +6,16 @@ from frappe.model.document import Document
 
 class ThemeSettings(Document):
 	pass
+
+
+
+@frappe.whitelist(allow_guest=True)
+def get_theme_settings():
+    doc = frappe.get_single("Theme Settings")
+    return {
+        "card_image": doc.card_image,
+        "powered_by": doc.powered_by,
+        "link": doc.link,
+        "login_desc": doc.login_desc,
+        "login_head": doc.login_head
+    }
