2eca72783fd17a9e19868853e41a287da43689a8
shayanfh <shayan.famous79@gmail.com>
2026-04-29 13:58:27 +0330

    add frappe import

--- datavalue_theme_15/datavalue_theme_15/doctype/theme_settings/theme_settings.py
+++ datavalue_theme_15/datavalue_theme_15/doctype/theme_settings/theme_settings.py
@@ -1,11 +1,12 @@
 # Copyright (c) 2021, Abdo Hamoud and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class ThemeSettings(Document):
 	pass
+
 
 
 
